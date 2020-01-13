55d280cac013b58eb2e1b832a23119aae1978861
Valeri Sokolov <valeri.sokolov@derivco.se>
2020-01-13 09:59:03 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AMQPX.MixProject do
   def project do
     [
       app: :ulfnet_amqpx,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
